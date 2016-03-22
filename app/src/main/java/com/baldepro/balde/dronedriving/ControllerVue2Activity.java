5e54607441d71b5393713c44c71791164bbab188
anounes <ablayeaas@gmail.com>
2016-03-22 16:53:55 +0100

    Initialisation du premier waypoint qui correspond au point de départ sur le port des minimes Trajectoire définie au clic sur l'écran Sauvegarde des waypoints obtenus dans la liste des waypoints pour préparer la transmission à la vue 3

--- app/src/main/java/com/baldepro/balde/dronedriving/ControllerVue2Activity.java
+++ app/src/main/java/com/baldepro/balde/dronedriving/ControllerVue2Activity.java
@@ -1,7 +1,10 @@
 package com.baldepro.balde.dronedriving;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -9,11 +12,17 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
-public class ControllerVue2Activity extends FragmentActivity implements OnMapReadyCallback {
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class ControllerVue2Activity extends FragmentActivity implements OnMapReadyCallback, Observer, View.OnClickListener {
 
     private GoogleMap mMap;
-
+    private ModeleVue2 modeleVue2;
+    private LatLng init;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,14 +31,42 @@ public class ControllerVue2Activity extends FragmentActivity implements OnMapRea
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        modeleVue2 = new ModeleVue2();
+        modeleVue2.addObserver(this);
+        init = new LatLng(46.155, -1.155);
+
     }
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         // anoune a modifier quelques choses
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng minimes = new LatLng(46.155, -1.155);
+        mMap.addMarker(new MarkerOptions().position(minimes).title("Marker in port des Minimes"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(minimes));
+        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                modeleVue2.setModeleVue2(latLng);
+            }
+        });
+    }
+
+    @Override
+    public void update(Observable observable, Object data) {
+        /*methode qui d�finie la modification de la vue par le modele*/
+        if(observable instanceof ModeleVue2){
+           ModeleVue2  modele2 = (ModeleVue2)data;
+           mMap.addPolyline(new PolylineOptions().geodesic(true)
+           .add(init)
+           .add(modele2.getLast().getWayPoint()));
+            init = modele2.getLast().getWayPoint();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
